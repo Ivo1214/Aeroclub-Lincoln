2daa418cb67b2428536c836e81c98014a3c47e25
Ivo1214 <ivo1214@live.com>
2023-12-07 10:33:00 -0300

    Update SaldoActual.tsx

--- src/components/card-saldo-actual/SaldoActual.tsx
+++ src/components/card-saldo-actual/SaldoActual.tsx
@@ -4,16 +4,17 @@ import Typography from '@mui/material/Typography';
 import "./saldo-actual.css"
 import { apiCuentaCorriente } from '../../services/apiCuentaCorriente';
 import { useEffect, useState } from 'react';
+import { apiUsuarios } from '../../services/apiUsuarios';
 
 export default function SaldoActual() {
   const [saldo, setSaldo] = useState<number | null>(null);
 
-  const getSaldo = async (id: number) => {
+  const getSaldo = async () => {
     try {
-      const response = await apiCuentaCorriente.getById(id);
+      const email = await apiUsuarios.getUserByEmail(sessionStorage.getItem("email") as string);
+      const response = await apiCuentaCorriente.getById(email.respuesta.id_usuarios);
       // Parsea la respuesta a un entero y actualiza el estado
       setSaldo(parseInt(response) || 0); // Si no se puede parsear, establece el saldo en 0
-      alert("Falta buscar el saldo del usuario, el valor cargado es de muestra.");
     } catch (error: any) {
       console.log(error.message);
       setSaldo(null); // Manejo de error, establece el saldo en null
@@ -21,7 +22,7 @@ export default function SaldoActual() {
   };
 
   useEffect(() => {
-    getSaldo(2);
+    getSaldo();
   }, []);
 
   return (
